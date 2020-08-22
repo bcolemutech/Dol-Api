5181ef5177a863f3dfca9d5fb5cf3d97f4262125
bcolemutech <bcolemutech@hotmail.com>
2020-08-22 18:15:47 -0500

    Firebase authentication

--- DolApi/Startup.cs
+++ DolApi/Startup.cs
@@ -1,17 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AspNetCore.Firebase.Authentication.Extensions;
 using FirebaseAdmin;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace DolApi
 {
@@ -22,16 +15,15 @@ namespace DolApi
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             FirebaseApp.Create();
             services.AddMvc(options => options.EnableEndpointRouting = false);
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme,
-                    options => Configuration.Bind("JetSettings", options));
+            services.AddFirebaseAuthentication(Configuration["FirebaseAuthentication:Issuer"],
+                Configuration["FirebaseAuthentication:Audience"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
