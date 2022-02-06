71e85b60a82299ce0934cf30e7cb7500e1e1b995
bcolemutech <bcolemutech@hotmail.com>
2022-02-06 17:07:02 -0600

    Test cleanup and alteration for IEnumerable

--- DolApiTest/Controllers/AreaControllerTest.cs
+++ DolApiTest/Controllers/AreaControllerTest.cs
@@ -1,6 +1,6 @@
 ﻿namespace DolApiTest.Controllers
 {
-    using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
     using DolApi.Controllers;
     using DolApi.POCOs;
@@ -20,17 +20,17 @@
             _areaRepo = Substitute.For<IAreaRepo>();
             _sut = new AreaController(_areaRepo);
         }
-        
+
         [Fact]
         public async Task GetReturnsOkObjectIfFound()
         {
             const int x = 1;
             const int y = 1;
-            
-            var area = new Area{ X = x, Y = y};
+
+            var area = new Area { X = x, Y = y };
 
             _areaRepo.Retrieve(x, y).Returns(Task.FromResult(area));
-            
+
             var actual = await _sut.Get(x, y);
 
             await _areaRepo.Received(1).Retrieve(Arg.Is(x), Arg.Is(y));
@@ -48,7 +48,7 @@
             const int y = 1;
 
             _areaRepo.Retrieve(x, y).Returns(Task.FromResult<Area>(null));
-            
+
             var actual = await _sut.Get(x, y);
 
             await _areaRepo.Received(1).Retrieve(Arg.Is(x), Arg.Is(y));
@@ -61,8 +61,8 @@
         {
             const int x = 1;
             const int y = 1;
-            
-            var inputArea = new Area{ X = x, Y = y};
+
+            var inputArea = new Area { X = x, Y = y };
 
             var actual = await _sut.Put(x, y, inputArea);
 
@@ -76,8 +76,8 @@
         {
             const int x = 1;
             const int y = 1;
-            
-            var areaBad = new Area{ X = x, Y = 5};
+
+            var areaBad = new Area { X = x, Y = 5 };
 
             var actual = await _sut.Put(x, y, areaBad);
 
@@ -89,7 +89,7 @@
         [Fact]
         public async Task GetAllReturnsOkObjectWithList()
         {
-            var areas = new Area[]
+            IEnumerable<Area> areas = new List<Area>
             {
                 new() { X = 1, Y = 1 },
                 new() { X = 1, Y = 2 },
@@ -104,8 +104,8 @@
             await _areaRepo.Received(1).RetrieveAll();
 
             actual.Should().BeOfType<OkObjectResult>();
-            actual.As<OkObjectResult>().Value.Should().BeOfType<Area[]>();
-            actual.As<OkObjectResult>().Value.As<Area[]>().Should().HaveCount(4);
+            actual.As<OkObjectResult>().Value.Should().BeOfType<List<Area>>();
+            actual.As<OkObjectResult>().Value.As<List<Area>>().Should().HaveCount(4);
         }
     }
 }
