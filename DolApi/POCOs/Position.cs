dcb82bfc5902c91cdbdaeb69bdbea6576ce0ebcf
bcolemutech <bcolemutech@hotmail.com>
2022-03-10 22:33:22 -0600

    New position type Firestore conversion

---
+++ DolApi/POCOs/Position.cs
@@ -0,0 +1,34 @@
+﻿namespace DolApi.POCOs;
+
+using dol_sdk.Enums;
+using dol_sdk.POCOs;
+using Google.Cloud.Firestore;
+
+[FirestoreData]
+public class Position : IPosition
+{
+
+    public Position(IPosition value)
+    {
+        Action = value.Action;
+        Location = value.Location;
+        Populace = value.Populace;
+        X = value.X;
+        Y = value.Y;
+    }
+
+    [FirestoreProperty]
+    public int X { get; set; }
+
+    [FirestoreProperty]
+    public int Y { get; set; }
+
+    [FirestoreProperty]
+    public string Populace { get; set; }
+
+    [FirestoreProperty]
+    public string Location { get; set; }
+
+    [FirestoreProperty]
+    public Action Action { get; set; }
+}
