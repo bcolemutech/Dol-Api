86717c82dca7a3312fd4897d33f5bc2275d93e31
BCole <bcolemutech@hotmail.com>
2022-03-17 23:15:01 -0500

    Use full User POCO to save doc

--- DolApi/Repositories/PlayerRepo.cs
+++ DolApi/Repositories/PlayerRepo.cs
@@ -1,36 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
+﻿namespace DolApi.Repositories;
+
+using System;
 using System.Threading.Tasks;
 using Google.Cloud.Firestore;
 using Microsoft.Extensions.Configuration;
+using dol_sdk.POCOs;
 
-namespace DolApi.Repositories
+public interface IPlayerRepo
 {
-    public interface IPlayerRepo
+    Task Add(string userId, PlayerRequest playerRequest);
+}
+
+public class PlayerRepo : IPlayerRepo
+{
+    private const string Players = "players";
+    private readonly FirestoreDb _db;
+
+    public PlayerRepo(IConfiguration configuration)
     {
-        Task Add(string userId);
+        _db = FirestoreDb.Create(configuration["ProjectId"]);
     }
 
-    public class PlayerRepo : IPlayerRepo
+    public async Task Add(string userId, PlayerRequest playerRequest)
     {
-        private const string Players = "players";
-        private readonly FirestoreDb _db;
+        var docRef = _db.Collection(Players).Document(userId);
+        var snapshot = await docRef.GetSnapshotAsync();
 
-        public PlayerRepo(IConfiguration configuration)
+        User user;
+        if (snapshot.Exists)
         {
-            _db = FirestoreDb.Create(configuration["ProjectId"]);
+            user = snapshot.ConvertTo<User>();
+            user.Authority = playerRequest.Authority;
         }
-
-        public async Task Add(string userId)
+        else
         {
-            var docRef = _db.Collection(Players).Document(userId);
-            var user = new Dictionary<string, object>
+            user = new User
             {
-                { "UserId", userId }
+                Authority = playerRequest.Authority,
+                Email = playerRequest.Email,
+                UserId = userId
             };
-            var result = await docRef.SetAsync(user, SetOptions.MergeAll);
-            
-            Console.WriteLine($"New Player doc added at {result.UpdateTime}");
         }
+
+        var result = await docRef.SetAsync(user, SetOptions.MergeAll);
+
+        Console.WriteLine($"New Player doc added at {result.UpdateTime}");
     }
 }
