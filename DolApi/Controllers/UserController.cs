c922baf95f848d3830f90c6beeba293bd0ea0881
bcolemutech <bcolemutech@hotmail.com>
2022-04-10 22:28:36 -0500

    Patch method to update player fields

--- DolApi/Controllers/UserController.cs
+++ DolApi/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DolApi.Repositories;
 using DolApi.Services;
@@ -9,20 +10,27 @@ using FirebaseAdmin.Auth;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using dol_sdk.POCOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 
-[Authorize(Policy = "Admin")]
+
 [Route("[controller]")]
 public class UserController
 {
     private readonly IAdminService _admin;
     private readonly IPlayerRepo _playerRepo;
+    private readonly string _userId;
 
-    public UserController(IAdminService adminService, IPlayerRepo playerRepo)
+    public UserController(IHttpContextAccessor httpContextAccessor,IAdminService adminService, IPlayerRepo playerRepo)
     {
         _admin = adminService;
         _playerRepo = playerRepo;
+        
+        var user = httpContextAccessor.HttpContext?.User;
+        _userId = user?.Claims.First(c => c.Type == "user_id").Value;
     }
 
+    [Authorize(Policy = "Admin")]
     [HttpPost]
     public async Task<IActionResult> Post([FromBody] PlayerRequest playerRequest)
     {
@@ -60,6 +68,28 @@ public class UserController
             { "Authority", playerRequest.Authority },
         };
         await _admin.SetCustomUserClaimsAsync(userId, claims);
+
+        return new OkResult();
+    }
+
+    [Authorize(Policy = "Player")]
+    [HttpPatch]
+    public async Task<IActionResult> Patch(JsonPatchDocument<User> doc)
+    {
+        var user = await _playerRepo.Get(_userId);
+
+        if (user is null)
+        {
+            return new NotFoundResult();
+        }
+
+        var resetSession = doc.Operations.Any(x => x.path == nameof(User.CurrentCharacter));
+
+        doc.ApplyTo(user);
+
+        user.SessionId = resetSession ? string.Empty : user.SessionId;
+        
+        await _playerRepo.Update(_userId, user);
 
         return new OkResult();
     }
